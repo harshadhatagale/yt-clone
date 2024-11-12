03065b49710332ffa92a61c45947fdf8bc4f872a
harshadhatagale <harshadhatagale6@gmail.com>
2024-11-12 09:32:03 +0530

    navbar

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -10,6 +10,8 @@ import { FaRegBell } from "react-icons/fa";
 import { IoIosMenu } from "react-icons/io";
 import { RiAccountCircleLine } from "react-icons/ri";
 import { CiSearch } from "react-icons/ci";
+import { Menu } from 'lucide-react'
+
 const roboto = Roboto({
     subsets: ["latin"],
     weight: ['100', '300', '400', '500', '700', '900']
@@ -22,22 +24,23 @@ export default function TopNav() {
         setSearchValue(target.value)
     }
     return (
-        <nav className='px-7 dark:bg-black dark:text-white py-2 w-full hi-16 flex justify-between items-center'>
+        <nav className='lg:px-4 px-2 dark:bg-black dark:text-white py-1 w-full hi-16 flex justify-between items-center'>
             <div className='flex justify-center items-center space-x-3'>
-                <IoIosMenu size={45} className='hover:bg-gray-200 hidden lg:block dark:hover:bg-zinc-600 p-2 rounded-full' />
+                <Menu size={35} className='hover:bg-gray-200 hidden lg:block dark:hover:bg-zinc-800 cursor-pointer rounded-full' />
                 <Link className='flex justify-center h-full items-center' href={''}>
                     <Image src={logo} alt='logo' width={50} height={50} />
-                    <p className={`${roboto.className} font-semibold text-xl tracking-tighter`}>Youtube <sup className='font-normal'>In</sup></p>
+                    <p className={`${roboto.className} font-semibold text-xl tracking-wide`}>Youtube <sup className='font-normal'>In</sup></p>
                 </Link>
             </div>
-            <div className='search_bar lg:block hidden outline outline-[0.5px] outline-gray-200 dark:outline-zinc-800 w-96 h-10 rounded-tr-xl rounded-tl-xl rounded-br-xl rounded-bl-xl'>
-                <div className='flex w-full h-full justify-center items-center '>
-                    <button title='Search' className='w-[15%] flex justify-center items-center dark:bg-zinc-800 bg-gray-200 h-full rounded-tl-xl rounded-bl-xl'><CiSearch size={20}/></button>
-                    <input type="search" className='w-[70%] h-full outline-none bg-transparent bg-white px-3 text-base' value={searchValue} placeholder="Search" onChange={searchTextHandler} />
-                    <button title='Search' className='w-[15%] flex justify-center items-center dark:bg-zinc-800 bg-gray-200 h-full rounded-tr-xl rounded-br-xl'><CiSearch size={20}/></button>
+            <div className='search_bar lg:block hidden outline outline-[0.5px] border-[1px] border-zinc-600 outline-gray-200 dark:outline-zinc-800 w-96 h-10 rounded-3xl'>
+                <div className='flex w-full h-full justify-center items-center bg-transparent'>
+                    <button title='Search' className='w-[15%] flex justify-center border-[1px] border-zinc-600 items-center dark:bg-zinc-800 bg-gray-200 h-full rounded-tl-3xl rounded-bl-3xl'><CiSearch size={20}/></button>
+                    <input type="search" className={`w-[70%] h-full outline-none focus:border-blue-600 dark:bg-zinc-800 border-[1px] border-zinc-600 bg-transparent bg-white px-3 text-base ${roboto.className}`} value={searchValue} placeholder="Search" onChange={searchTextHandler} />
+                    <button title='Search' className='w-[15%] flex justify-center border-[1px] border-zinc-600 items-center dark:bg-zinc-800 bg-gray-200 h-full rounded-tr-3xl rounded-br-3xl'><CiSearch size={20}/></button>
                 </div>
             </div>
-            <div className='flex justify-center items-center space-x-7'>
+            <Link href={'/'} className='lg:hidden'><CiSearch size={20}/></Link>
+            <div className='justify-center items-center space-x-7 hidden lg:flex'>
                 <Link href={"/"} title='Create'><MdOutlineVideoCall size={30} /></Link>
                 <Link href={"/"} title='Notifications'><FaRegBell size={20} /></Link>
                 <Link href={"/"} title='Account'><RiAccountCircleLine size={25} /></Link>
