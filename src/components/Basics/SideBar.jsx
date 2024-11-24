de661d64626c6f6bfc21364e18f18b2122103dae
harshadhatagale <harshadhatagale6@gmail.com>
2024-11-24 14:35:38 +0530

    theming

--- src/components/Basics/SideBar.jsx
+++ src/components/Basics/SideBar.jsx
@@ -6,11 +6,11 @@ import { RiAccountCircleLine } from "react-icons/ri";
 import Link from 'next/link'
 export default function SideBar({isOpen}) {
     return (
-        <div className={`fixed w-full lg:flex-col justify-around lg:justify-start items-center flex h-16 bottom-0 lg:w-16 lg:h-full lg:left-0 lg:top-16 transition-transform transform bg-black ${isOpen? "-translate-x-full": "translate-x-0"}`}>
-            <div title='Home' className='w-full h-16 rounded-md py-2 space-y-2 hover:bg-zinc-800 flex justify-center items-center flex-col cursor-pointer'><House color='white' size={20} /><span className='text-[10px] font-bold tracking-tighter text-white'>Home</span></div>
-            <div title='Shorts' className='w-full h-16 rounded-md py-2 space-y-2 hover:bg-zinc-800 flex justify-center items-center flex-col cursor-pointer'><SiYoutubeshorts color='white' size={20} /><span className='text-[10px] font-bold tracking-tighter text-white'>Shorts</span></div>
-            <div title='Subscriptions' className='w-full h-16 rounded-md py-2 space-y-2 hover:bg-zinc-800 flex justify-center items-center flex-col cursor-pointer'><MdOutlineSubscriptions color='white' size={20} /><span className='text-[10px] font-bold text-white'>Subscriptions</span></div>
-            <div title='Subscriptions' className='w-full h-16 rounded-md py-2 space-y-2 hover:bg-zinc-800 flex justify-center items-center flex-col cursor-pointer'><RiAccountCircleLine color='white' size={20} /><span className='text-[10px] font-bold text-white'>You</span></div>
+        <div className={`fixed w-full lg:flex-col justify-around lg:justify-start items-center flex h-16 bottom-0 lg:w-16 lg:h-full lg:left-0 lg:top-16 transition-transform transform dark:bg-black bg-white ${isOpen? "-translate-x-full": "translate-x-0"}`}>
+            <div title='Home' className='w-full h-16 rounded-md py-2 space-y-2 lg:hover:bg-gray-200 lg:dark:hover:bg-zinc-800 text-black dark:text-white flex justify-center items-center flex-col cursor-pointer'><House className='text-black dark:text-white' size={20} /><span className='text-[10px] font-bold tracking-tighter text-black dark:text-white'>Home</span></div>
+            <div title='Shorts' className='w-full h-16 rounded-md py-2 space-y-2 lg:hover:bg-gray-200 lg:dark:hover:bg-zinc-800 text-black dark:text-white flex justify-center items-center flex-col cursor-pointer'><SiYoutubeshorts className='text-black dark:text-white' size={20} /><span className='text-[10px] font-bold tracking-tighter text-black dark:text-white'>Shorts</span></div>
+            <div title='Subscriptions' className='w-full h-16 rounded-md py-2 space-y-2 lg:hover:bg-gray-200 lg:dark:hover:bg-zinc-800 text-black dark:text-white flex justify-center items-center flex-col cursor-pointer'><MdOutlineSubscriptions className='text-black dark:text-white' size={20} /><span className='text-[10px] font-bold text-black dark:text-white'>Subscriptions</span></div>
+            <div title='Subscriptions' className='w-full h-16 rounded-md py-2 space-y-2 lg:hover:bg-gray-200 lg:dark:hover:bg-zinc-800 text-black dark:text-white flex justify-center items-center flex-col cursor-pointer'><RiAccountCircleLine className='text-black dark:text-white' size={20} /><span className='text-[10px] font-bold text-black dark:text-white'>You</span></div>
         </div>
     )
 }
--- src/components/BottomSideNav.jsx
+++ src/components/BottomSideNav.jsx
@@ -5,13 +5,21 @@ import { SiYoutubeshorts } from "react-icons/si";
 import { House } from 'lucide-react'
 import { MdOutlineSubscriptions } from "react-icons/md";
 import Link from 'next/link'
+import { LuHistory } from "react-icons/lu";
+import { FaChevronRight } from "react-icons/fa";
 export default function BottomSideNav({ isOpen }) {
   return (
-    <div className={`hidden transition-transform lg:w-52 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} items-center justify-around lg:justify-normal fixed bottom-0 lg:top-16 lg:left-0 flex lg:flex-col lg:h-full px-4 shadow-lg`}>
-      <div className='w-full flex lg:flex-col'>
-        <Link href={"/"} className='lg:hover:bg-zinc-800 shadow-md py-2 flex flex-col lg:flex-row justify-start items-center rounded-md px-2 w-full text-sm text-white space-x-4'><House color='white' size={18} /><span className='text-sm font-semibold'>Home</span></Link>
-        <Link href={"/"} className='lg:hover:bg-zinc-800 shadow-md py-2 flex flex-col lg:flex-row justify-start items-center rounded-md px-2 w-full text-sm text-white space-x-4'><SiYoutubeshorts color='white' size={18} /><span className='text-sm font-semibold'>Shorts</span></Link>
-        <Link href={"/"} className='lg:hover:bg-zinc-800 shadow-md py-2 flex flex-col lg:flex-row justify-start items-center rounded-md px-2 w-full text-sm text-white space-x-4'><MdOutlineSubscriptions color='white' size={18} /><span className='tracking-tighter text-sm font-semibold'>Subscriptions</span></Link>
+    <div className={`hidden lg:block transition-transform lg:w-52 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} items-center justify-around lg:justify-normal fixed bottom-0 lg:top-16 lg:left-0 flex lg:flex-col lg:h-full px-4 shadow-lg`}>
+      <div className='w-full flex lg:flex-col py-1'>
+        <Link href={"/"} className='dark:hover:bg-zinc-800 hover:bg-gray-200 text-black py-3 flex flex-col lg:flex-row justify-start items-center rounded-md px-2 w-full text-sm dark:text-white space-x-4'><House size={18}  className='text-black dark:white'/><span className='text-sm font-semibold'>Home</span></Link>
+        <Link href={"/"} className='dark:hover:bg-zinc-800 hover:bg-gray-200 text-black py-3 flex flex-col lg:flex-row justify-start items-center rounded-md px-2 w-full text-sm dark:text-white space-x-4'><SiYoutubeshorts size={18}  className='text-black dark:white'/><span className='text-sm font-semibold'>Shorts</span></Link>
+        <Link href={"/"} className='dark:hover:bg-zinc-800 hover:bg-gray-200 text-black py-3 flex flex-col lg:flex-row justify-start items-center rounded-md px-2 w-full text-sm dark:text-white space-x-4'><MdOutlineSubscriptions size={18}  className='text-black dark:white'/><span className='tracking-tighter text-sm font-semibold'>Subscriptions</span></Link>
+      </div>
+      <div>
+        <Link href={"/"} className='dark:hover:bg-zinc-800 hover:bg-gray-200 text-black py-3 flex flex-col lg:flex-row justify-start items-center rounded-md px-2 w-full text-sm dark:text-white space-x-4'><span className='text-sm font-semibold'>You</span><FaChevronRight className='text-black dark:white' size={10} /></Link>
+        <Link href={"/"} className='dark:hover:bg-zinc-800 hover:bg-gray-200 text-black py-3 flex flex-col lg:flex-row justify-start items-center rounded-md px-2 w-full text-sm dark:text-white space-x-4'><LuHistory size={18}  className='text-black dark:white'/><span className='text-sm font-semibold'>History</span></Link>
+        <Link href={"/"} className='dark:hover:bg-zinc-800 hover:bg-gray-200 text-black py-3 flex flex-col lg:flex-row justify-start items-center rounded-md px-2 w-full text-sm dark:text-white space-x-4'><House size={18}  className='text-black dark:white'/><span className='text-sm font-semibold'>Playlist</span></Link>
+        <Link href={"/"} className='dark:hover:bg-zinc-800 hover:bg-gray-200 text-black py-3 flex flex-col lg:flex-row justify-start items-center rounded-md px-2 w-full text-sm dark:text-white space-x-4'><House size={18}  className='text-black dark:white'/><span className='text-sm font-semibold'>Playlist</span></Link>
       </div>
     </div>
   )
