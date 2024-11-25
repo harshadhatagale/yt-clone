7b1a833ff4bb6a6f093b575b6e6e6573788572da
harshadhatagale <harshadhatagale6@gmail.com>
2024-11-25 19:32:34 +0530

    filter chips

--- src/components/Chips.jsx
+++ src/components/Chips.jsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { useRef } from "react";
 import { FaAngleRight } from "react-icons/fa6";
 import { FaAngleLeft } from "react-icons/fa6";
+import { MdOutlineExplore } from "react-icons/md";
 export default function Chips() {
     const scrollContainerRef = useRef(null);
     const scrollPrev = () => {
@@ -68,17 +69,18 @@ export default function Chips() {
                 >
                     <FaAngleLeft />
                 </button>
-
-                {/* Scrollable Chips Container */}
                 <div
                     ref={scrollContainerRef}
-                    className="flex p-3 w-[94%] space-x-3 justify-start items-center overflow-x-auto lg:overflow-x-hidden snap-x snap-mandatory rounded-lg"
+                    className="flex p-3 w-90% lg:w-[94%] space-x-3 justify-start items-center overflow-x-auto lg:overflow-x-hidden lg:snap-x lg:snap-mandatory rounded-lg"
                 >
+                    <div className='text-white text-base hover:dark:bg-white hover:dark:text-black flex dark:bg-zinc-800 font-semibold dark:text-white cursor-pointer rounded-md shadow-md p-[2px] bg-black hover:bg-gray-200'>
+                        <MdOutlineExplore color='white' size={25} />
+                    </div>
                     {chipsData.map((filter, index) => (
                         <Link
-                        href={`?filter=${filter}`}
+                            href={`?filter=${filter}`}
                             key={index}
-                            className="text-white hover:dark:bg-white hover:dark:text-black flex text-nowrap dark:bg-zinc-800 font-semibold dark:text-white cursor-pointer px-3 rounded-md shadow-md py-1 bg-black snap-start flex-shrink-0 hover:bg-gray-200"
+                            className="text-white text-base hover:dark:bg-white hover:dark:text-black flex text-nowrap dark:bg-zinc-800 font-semibold dark:text-white cursor-pointer px-3 rounded-md shadow-md py-[2px] bg-black snap-start flex-shrink-0 hover:bg-gray-200"
                         >
                             {filter}
                         </Link>
@@ -86,12 +88,12 @@ export default function Chips() {
                 </div>
 
                 {/* Next Button */}
-                    <button
-                        onClick={scrollNext}
-                        className="absolute hidden lg:block right-0 top-1/2 -translate-y-1/2 z-10 p-2 rounded-full shadow-md dark:text-white hover:bg-zinc-800"
-                    >
-                        <FaAngleRight />
-                    </button>
+                <button
+                    onClick={scrollNext}
+                    className="absolute hidden lg:block right-0 top-1/2 -translate-y-1/2 z-10 p-2 rounded-full shadow-md dark:text-white hover:bg-zinc-800"
+                >
+                    <FaAngleRight />
+                </button>
             </div>
         </div>
     )
